d32538f06736eeb218a19a3a246d40b09ca28943
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-12-25 10:21:34 +0300

    use glightbox

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,7 +1,9 @@
-import { h, nextTick, watch } from "vue";
+import { h, nextTick, watch, onMounted } from "vue";
 import DefaultTheme from 'vitepress/theme-without-fonts'
-import { useData } from "vitepress";
+import { useData, useRoute } from "vitepress";
 import { createMermaidRenderer } from "vitepress-mermaid-renderer";
+import GLightbox from 'glightbox';
+import 'glightbox/dist/css/glightbox.css';
 import './style.css'
 
 export default {
@@ -25,4 +27,42 @@ export default {
         );
         return h(DefaultTheme.Layout);
     },
+    setup() {
+        const route = useRoute();
+        let lightbox = null;
+
+        const initGLightbox = () => {
+            if (lightbox) {
+                lightbox.destroy();
+            }
+
+            nextTick(() => {
+                const images = document.querySelectorAll('.vp-doc img');
+
+                images.forEach((img) => {
+                    if (!img.parentElement.classList.contains('glightbox-wrapper')) {
+                        const wrapper = document.createElement('a');
+                        wrapper.href = img.src;
+                        wrapper.className = 'glightbox';
+                        wrapper.setAttribute('data-glightbox', `title: ${img.alt || ''}`);
+
+                        img.parentNode.insertBefore(wrapper, img);
+                        wrapper.appendChild(img);
+                    }
+                });
+
+                lightbox = GLightbox({
+                    selector: '.glightbox',
+                });
+            });
+        };
+
+        onMounted(() => {
+            initGLightbox();
+        });
+
+        watch(() => route.path, () => {
+            initGLightbox();
+        });
+    }
 }
