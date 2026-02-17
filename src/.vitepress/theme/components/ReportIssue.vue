223b541e6ab49cf9933416ccec4355658f45a03c
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-02-17 22:44:00 +0300

    add link

---
+++ src/.vitepress/theme/components/ReportIssue.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useData } from 'vitepress'
+
+const { lang } = useData()
+
+const labels = {
+  'en': 'Report an issue',
+  'es': 'Reportar un problema',
+  'id': 'Laporkan masalah',
+  'it': 'Segnala un problema',
+  'ru': 'Сообщить об ошибке',
+  'zh-CN': '报告问题',
+}
+</script>
+
+<template>
+  <div class="report-issue">
+    <a
+      href="https://github.com/yiisoft/docs/issues"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="report-issue-link"
+    >
+      <span class="vpi-report-issue-icon" />
+      {{ labels[lang] || labels['en'] }}
+    </a>
+  </div>
+</template>
